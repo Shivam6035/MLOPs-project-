137df8e1a3944963e3483a0f22baff1076f76177
Shivam6035 <shivamzack6035@gmail.com>
2026-05-19 23:35:48 +0530

    project structured reday venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shivam Kumar",
+    author_email="shivamkumar6035773gmail.com",
+    packages=find_packages()
+)
